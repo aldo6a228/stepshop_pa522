c5298170a006db69f9f78a510b33d1b40e859e10
aldo6a228 <itstart@step.kz>
2025-05-06 18:20:01 +0500

    lesson_05:models

--- stepshop/urls.py
+++ stepshop/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 
@@ -25,3 +27,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls', namespace='mainapp')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT)
